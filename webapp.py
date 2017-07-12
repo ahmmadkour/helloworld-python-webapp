7868c82cdb85583e5172d5aa99e812a48882116e
Ahmed Madkour <ahm.madkour@gmail.com>
2017-07-12 19:44:40 +0000

    Get welcome message from local mysql

--- webapp.py
+++ webapp.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'helloworld'
+app.config['MYSQL_HOST'] = '127.0.0.1'
+
+mysql = MySQL(app)
 
 
 @app.route("/")
 def main():
-    return "Hello, world"
+    cur = mysql.connection.cursor()
+    query="SELECT value FROM webapp where msg = %s"
+    param="index"
+    cur.execute(query, [param])
+    row = cur.fetchone()
+    return row
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
