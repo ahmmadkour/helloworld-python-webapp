e7f3cb6c7bcb72ddfbb61b18c46112c796c2c57f
Ahmed Madkour <ahm.madkour@gmail.com>
2017-07-12 20:26:02 +0000

    Add python webapp dockerfile

--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'helloworld'
-app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_HOST'] = 'mysql'
 
 mysql = MySQL(app)
 
