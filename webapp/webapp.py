d0f1dc9a8f2194f0ea5c9c73829c85994b3d2dc4
Ahmed Madkour <ahm.madkour@gmail.com>
2017-07-14 19:17:21 +0000

    Add port discovery for the db

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+import dns.resolver
 
 app = Flask(__name__)
+
+MYSQL_HOST = 'mysql.service.consul'
+
+# Get MySQL port
+answers = dns.resolver.query(MYSQL_HOST, 'SRV')
+MYSQL_PORT = answers[0].port
 
 # MySQL configurations
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'helloworld'
-app.config['MYSQL_HOST'] = 'mysql.service.consul'
+app.config['MYSQL_HOST'] = MYSQL_HOST
+app.config['MYSQL_PORT'] = MYSQL_PORT
 
 mysql = MySQL(app)
 
